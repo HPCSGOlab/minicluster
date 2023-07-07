fb8c7b788e298924ae0712d3ae35dc2dd0cc5881
Tyler Allen <t.allen@uncc.edu>
2023-07-07 16:32:51 -0400

    changing network configuration

--- compute/network.sh
+++ compute/network.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-# The LAN IP and subnet mask.
-LAN_IP="192.168.0.1"
-LAN_NETMASK="255.255.255.0"
-LAN_SUBNET="192.168.0.0/24"
+# Name of the connection/device
+CONN_NAME="Wired connection 1"
+DEV_NAME="eth0"
 
-# The wireless network name (SSID).
-SSID="NinerWiFi-Guest"
+# Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
 
-# Configure eth0.
-nmcli con add type ethernet ifname eth0 ip4 $LAN_IP/24
-nmcli con mod eth0 ipv4.method manual ipv4.addresses $LAN_IP/24 ipv4.gateway $LAN_IP ipv4.dns "$LAN_IP"
-nmcli con up eth0
+# Set connection to use DHCP
+nmcli con mod "$CONN_NAME" ipv4.method auto
+nmcli con down "$CONN_NAME" && nmcli con up "$CONN_NAME"
 
-# Configure wlan0.
-nmcli dev wifi rescan
-nmcli dev wifi connect $SSID ifname wlan0
+# Wait a bit to make sure DHCP negotiation has completed
+sleep 5
 
-# Set up the routing table.
-ip route add default via $(nmcli -g IP4.GATEWAY dev show wlan0) dev wlan0 metric 100
-ip route add $LAN_SUBNET dev eth0 metric 50
+# Remove all existing routes
+ip route flush table main
+
+# Set the default gateway
+ip route add default via 192.168.0.10 dev "$DEV_NAME"
+
+# Exit the script
+exit 0
 
