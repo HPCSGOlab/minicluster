bd72564e63506da2597fa9139565dad2f10f0017
Tyler Allen <t.allen@charlotte.edu>
2024-05-22 17:21:22 -0400

    adding line to fix sudoers when we start; minor fixes to process issues

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,24 +2,10 @@
 
 DLNAME=jetson_linux_r36.3.0_aarch64.tbz2
 OD=`pwd`
+export MAKEFLAGS='-j'
 
-# KERNEL
-# this is  for jetpack 36.3. Will have to update this script in the future if we want to support future jetpacks; not sure
-# how consistent the naming conventions are
-wget https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v3.0/release/${DLNAME}
-tar xvf ${DLNAME}
-cd Linux_for_Tegra/source
-./source_sync.sh
-
-cp ${OD}/kernel/.config kernel/kernel-jammy-src/
-cp ${OD}/kernel/Makefile kernel/
-
-mkdir kernel_out
-./nvbuild.sh -o kernel_out
-#TODO check this actually replaces /boot/Image and /boot/initrd
-sudo ./nvbuild.sh -i
-cp /boot/Image ${OD}/root/srv/tftp/Image
-cd $OD
+sudo apt update
+sudo apt install -y build-essential bc libdwarf-dev libncurses-dev vim htop locate libssl-dev nfs-kernel-server tftpd-hpa  isc-dhcp-server ntp firefox
 
 #INITRD
 cd initrd
@@ -27,23 +13,49 @@ sudo rm -rf root
 mkdir -p root
 cd root
 sudo gzip -dc /boot/initrd | sudo cpio -idmv
-cp ../init root/
-find . | sudo cpio -o -H newc | sudo gzip -9 > ${OD}/root/srv/tftp/initrd
+sudo cp ../init .
+sudo find . | sudo cpio -o -H newc | sudo gzip -9 > ${OD}/root/srv/tftp/initrd
 cd $OD
 
-# this will do apt update
-sudo unminimize
+# KERNEL
+# this is  for jetpack 36.3. Will have to update this script in the future if we want to support future jetpacks; not sure
+# how consistent the naming conventions are
+dl_url=https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v3.0/release/${DLNAME}
 
-sudo apt install -y build-essential bc libdwarf libncurses-dev vim htop locate libssl-dev nfs-kernel-server tftpd-hpa  isc-dhcp-server ntp firefox
+if [[ ! -f ${DLNAME} ]]; then
+	wget ${dl_url}
+fi
+tar xvf ${DLNAME}
+cd Linux_for_Tegra/source
+printf "jetson_36.3\njetson_36.3\n" | ./source_sync.sh
+
+cp ${OD}/kernel/.config kernel/kernel-jammy-src/
+cp ${OD}/kernel/Makefile kernel/
+
+mkdir -p kernel_out
+./nvbuild.sh -o kernel_out
+#TODO check this actually replaces /boot/Image and /boot/initrd
+sudo ./nvbuild.sh -i
+cp /boot/Image ${OD}/root/srv/tftp/Image
+cd $OD
+
+
+# this will do apt update
+yes | sudo unminimize
 sudo systemctl enable nfs-kernel-server tftpd-hpa isc-dhcp-server  NetworkManager-wait-online.service ntp
 sudo updatedb
 
 sudo mkdir /etc/exports.d
 
-sudo systemctl restart nfs-kernel-server tftpd-hpa isc-dhcp-server  NetworkManager-wait-online.service ntp
-
-
 # copy configs over
 sudo cp -r root/* /
+
+sudo ip route add 192.168.0.0/24 dev eth0
+
+echo -e "demo ALL=(ALL) NOPASSWD: ALL\ndhcpd ALL=(ALL) NOPASSWD: /etc/dhcp/create_client_dirs.sh" | sudo tee /etc/sudoers.d/99-custom-sudoers
+sudo chmod 0440 /etc/sudoers.d/99-custom-sudoers
+
+
+sudo systemctl restart nfs-kernel-server tftpd-hpa isc-dhcp-server  NetworkManager-wait-online.service ntp
 
 echo "Finished setup without errors. Reboot to reflect changes..."
